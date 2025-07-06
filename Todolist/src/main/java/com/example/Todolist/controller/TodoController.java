50dc3f7d92bfe1d64ab9a435a2616d932eac38d1
VIPIN KARTHIK M B <144827867+Vipinkarthik@users.noreply.github.com>
2025-07-07 00:49:01 +0530

    Add files via upload

---
+++ Todolist/src/main/java/com/example/Todolist/controller/TodoController.java
@@ -0,0 +1,43 @@
+package com.example.Todolist.controller;
+
+import com.example.Todolist.model.Todo;
+import com.example.Todolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/todos")
+@CrossOrigin(origins = "*")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Todo> getTodoById(@PathVariable Long id) {
+        return todoService.getTodoById(id);
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoService.createTodo(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Long id, @RequestBody Todo todo) {
+        return todoService.updateTodo(id, todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        todoService.deleteTodo(id);
+    }
+}
