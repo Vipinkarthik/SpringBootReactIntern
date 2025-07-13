7578991eab2caab975e45ffd99d1c313ea9b5988
Vipin karthik M B <vipinkarthik.mb2023ece@sece.ac.in>
2025-07-14 00:36:09 +0530

    Day8-SpringBootReactIntern

---
+++ Day8-SpringBootJavaIntern/Day8-SpringBootJavaIntern/src/main/java/com/example/Day8_SpringBootJavaIntern/Day8SpringBootJavaInternApplication.java
@@ -0,0 +1,13 @@
+package com.example.Day7_SpringBootJavaIntern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day7SpringBootJavaInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day7SpringBootJavaInternApplication.class, args);
+	}
+
+}
---
+++ Day8-SpringBootJavaIntern/Day8-SpringBootJavaIntern/src/test/java/com/example/Day8_SpringBootJavaIntern/Day8SpringBootJavaInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Day7_SpringBootJavaIntern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day7SpringBootJavaInternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
